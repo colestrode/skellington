eee2fc28cbe05ab40f88df7a348ca71fec647033
Cole Furfaro-Strode <colestrode@gmail.com>
2016-10-04 14:56:35 -0400

    switches to standardjs for linting, add ci testing for node 6

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
-'use strict';
+'use strict'
 
-let Botkit = require('botkit');
-let _ = require('lodash');
+const Botkit = require('botkit')
+const _ = require('lodash')
 
 module.exports = (config) => {
-  _.defaults(config, {debug: false, plugins: []});
+  _.defaults(config, {debug: false, plugins: []})
 
   if (!Array.isArray(config.plugins)) {
-    config.plugins = [config.plugins];
+    config.plugins = [config.plugins]
   }
 
   let slackbotConfig = {
     debug: config.debug
-  };
-
-  if (config.storage) {
-    slackbotConfig.storage = config.storage;
   }
 
-  let controller = Botkit.slackbot(slackbotConfig);
+  if (config.storage) {
+    slackbotConfig.storage = config.storage
+  }
 
-  addHelpListeners(controller, config.plugins);
-  startServer(config, controller);
+  let controller = Botkit.slackbot(slackbotConfig)
+
+  addHelpListeners(controller, config.plugins)
+  startServer(config, controller)
 
   let bot = controller.spawn({
     token: config.slackToken
-  });
+  })
 
-  startRtm(config, controller, bot);
+  startRtm(config, controller, bot)
 
   // restart if disconnected
   controller.on('rtm_close', (bot) => {
-    controller.log('rtm closed, attempting to reconnect');
-    bot.startRTM((err) =>{
+    controller.log('rtm closed, attempting to reconnect')
+    bot.startRTM((err) => {
       if (err) {
-        logError(controller, err, 'could not reconnect to the rtm, shutting down');
-        process.exit(1);
+        logError(controller, err, 'could not reconnect to the rtm, shutting down')
+        process.exit(1)
       }
-    });
-  });
-};
-
+    })
+  })
+}
 
 /**
  * Starts an express server for slash commands
@@ -48,10 +47,10 @@ module.exports = (config) => {
  * @param config
  * @param controller
  */
-function startServer(config, controller) {
+function startServer (config, controller) {
   // TODO port will be required for multi-team auth
   if (config.port) {
-    controller.setupWebserver(config.port);
+    controller.setupWebserver(config.port)
   }
 }
 
@@ -62,27 +61,26 @@ function startServer(config, controller) {
  * @param botName
  * @param plugins
  */
-function addHelpListeners(controller, plugins) {
-
-  _.forEach(plugins, function(plugin) {
+function addHelpListeners (controller, plugins) {
+  _.forEach(plugins, function (plugin) {
     if (_.get(plugin, 'help.text') && _.get(plugin, 'help.command')) {
-      registerHelpListener(controller, plugin.help);
+      registerHelpListener(controller, plugin.help)
     }
-  });
+  })
 
-  controller.hears('^help$', 'direct_mention,direct_message', function(bot, message) {
+  controller.hears('^help$', 'direct_mention,direct_message', function (bot, message) {
     let helpCommands = _.chain(plugins)
       .filter((plugin) => !!_.get(plugin, 'help.command'))
       .map((plugin) => '`@' + bot.identity.name + ' help ' + _.get(plugin, 'help.command') + '`')
       .value()
-      .join('\n');
+      .join('\n')
 
     if (!helpCommands.length) {
-      return bot.reply(message, 'I can\'t help you with anything right now. I still like you though :heart:');
+      return bot.reply(message, 'I can\'t help you with anything right now. I still like you though :heart:')
     }
 
-    return bot.reply(message, 'Here are some things I can help you with:\n' + helpCommands);
-  });
+    return bot.reply(message, 'Here are some things I can help you with:\n' + helpCommands)
+  })
 }
 
 /**
@@ -90,31 +88,31 @@ function addHelpListeners(controller, plugins) {
  * @param controller
  * @param helpInfo
  */
-function registerHelpListener(controller, helpInfo) {
-  controller.hears('^help ' + helpInfo.command + '$', 'direct_mention,direct_message', function(bot, message) {
-    let replyText = helpInfo.text;
+function registerHelpListener (controller, helpInfo) {
+  controller.hears('^help ' + helpInfo.command + '$', 'direct_mention,direct_message', function (bot, message) {
+    let replyText = helpInfo.text
 
     if (typeof helpInfo.text === 'function') {
-      let helpOpts = _.merge({botName: bot.identity.name}, _.pick(message, ['team', 'channel', 'user']));
+      let helpOpts = _.merge({botName: bot.identity.name}, _.pick(message, ['team', 'channel', 'user']))
 
-      replyText = helpInfo.text(helpOpts);
+      replyText = helpInfo.text(helpOpts)
     }
 
-    bot.reply(message, replyText);
-  });
+    bot.reply(message, replyText)
+  })
 }
 
-function startRtm(config, controller, bot) {
+function startRtm (config, controller, bot) {
   bot.startRTM((err, connectedBot) => {
     if (err) {
-      logError(controller, err, 'Error connecting to RTM');
-      return process.exit(1); // need the return for tests which mock our process.exit
+      logError(controller, err, 'Error connecting to RTM')
+      return process.exit(1) // need the return for tests which mock our process.exit
     }
 
     _.forEach(config.plugins, (plugin) => {
-      plugin.init(controller, connectedBot, controller.webserver);
-    });
-  });
+      plugin.init(controller, connectedBot, controller.webserver)
+    })
+  })
 }
 
 /**
@@ -124,7 +122,7 @@ function startRtm(config, controller, bot) {
  * @param err
  * @param message
  */
-function logError(controller, err, message) {
-  controller.log(message);
-  controller.log(err);
+function logError (controller, err, message) {
+  controller.log(message)
+  controller.log(err)
 }
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,25 +1,24 @@
-'use strict';
+'use strict'
 
-let chai = require('chai');
-let expect = chai.expect;
-let sinon = require('sinon');
-let proxyquire = require('proxyquire').noCallThru();
-let _ = require('lodash');
+const chai = require('chai')
+const expect = chai.expect
+const sinon = require('sinon')
+const proxyquire = require('proxyquire').noCallThru()
+const _ = require('lodash')
 
-chai.use(require('sinon-chai'));
+chai.use(require('sinon-chai'))
 
-describe('Skellington', function() {
-  let skellington;
-  let botkitMock;
-  let controllerMock;
-  let expressAppMock;
-  let botMock;
-  let connectedBotMock;
-  let exitOrig;
+describe('Skellington', function () {
+  let skellington
+  let botkitMock
+  let controllerMock
+  let expressAppMock
+  let botMock
+  let connectedBotMock
+  let exitOrig
 
-  beforeEach(function() {
-
-    connectedBotMock = {'team_info': {id: 'rickandmorty'}, identity: {name: 'butter-passer'}};
+  beforeEach(function () {
+    connectedBotMock = {'team_info': {id: 'rickandmorty'}, identity: {name: 'butter-passer'}}
 
     botMock = {
       startRTM: sinon.stub(),
@@ -27,9 +26,9 @@ describe('Skellington', function() {
       identity: {
         name: 'gazorpazorp'
       }
-    };
+    }
 
-    expressAppMock = {};
+    expressAppMock = {}
 
     controllerMock = {
       hears: sinon.stub(),
@@ -38,138 +37,137 @@ describe('Skellington', function() {
       on: sinon.stub(),
       setupWebserver: sinon.stub(),
       webserver: expressAppMock
-    };
+    }
 
     botkitMock = {
       slackbot: sinon.stub().returns(controllerMock)
-    };
+    }
 
-    exitOrig = process.exit;
-    process.exit = sinon.stub();
+    exitOrig = process.exit
+    process.exit = sinon.stub()
 
     skellington = proxyquire('../index', {
       'botkit': botkitMock
-    });
-  });
+    })
+  })
 
-  afterEach(function() {
-    process.exit = exitOrig;
-  });
+  afterEach(function () {
+    process.exit = exitOrig
+  })
 
-  describe('init', function() {
+  describe('init', function () {
+    it('should initialize', function () {
+      skellington({slackToken: 'abc123'})
 
-    it('should initialize', function() {
-      skellington({slackToken: 'abc123'});
+      expect(botkitMock.slackbot).to.have.been.calledWith({debug: false})
+      expect(botMock.startRTM).to.be.called
+      expect(controllerMock.on).to.be.calledWithMatch('rtm_close')
+      expect(controllerMock.setupWebserver).not.to.be.called
+    })
 
-      expect(botkitMock.slackbot).to.have.been.calledWith({debug: false});
-      expect(botMock.startRTM).to.be.called;
-      expect(controllerMock.on).to.be.calledWithMatch('rtm_close');
-      expect(controllerMock.setupWebserver).not.to.be.called;
-    });
-
-    it('should allow passed in configs', function() {
-      let storageMock = {};
+    it('should allow passed in configs', function () {
+      let storageMock = {}
 
       skellington({
         slackToken: 'abc123',
         debug: true,
         port: 1234,
         storage: storageMock
-      });
+      })
 
-      expect(botkitMock.slackbot).to.have.been.calledWith({debug: true, storage: storageMock});
-      expect(controllerMock.spawn).to.have.been.calledWith({token: 'abc123'});
-      expect(controllerMock.setupWebserver).to.be.calledWith(1234);
-    });
-  });
+      expect(botkitMock.slackbot).to.have.been.calledWith({debug: true, storage: storageMock})
+      expect(controllerMock.spawn).to.have.been.calledWith({token: 'abc123'})
+      expect(controllerMock.setupWebserver).to.be.calledWith(1234)
+    })
+  })
 
-  describe('startRtm', function() {
-    let callback;
+  describe('startRtm', function () {
+    let callback
 
-    beforeEach(function() {
-      skellington({});
-      callback = botMock.startRTM.args[0][0];
-    });
+    beforeEach(function () {
+      skellington({})
+      callback = botMock.startRTM.args[0][0]
+    })
 
-    it('should exit if there is an error connecting', function() {
-      let error = new Error('GAZORPAZORP');
+    it('should exit if there is an error connecting', function () {
+      let error = new Error('GAZORPAZORP')
 
-      callback(error);
-      expect(controllerMock.log.callCount).to.equal(2);
-      expect(controllerMock.log.args[1][0]).to.equal(error);
-      expect(process.exit).to.be.calledWith(1);
-    });
+      callback(error)
+      expect(controllerMock.log.callCount).to.equal(2)
+      expect(controllerMock.log.args[1][0]).to.equal(error)
+      expect(process.exit).to.be.calledWith(1)
+    })
 
-    describe('initialize plugins', function() {
-      let plugin;
+    describe('initialize plugins', function () {
+      let plugin
 
-      beforeEach(function() {
-        botMock.startRTM.reset();
+      beforeEach(function () {
+        botMock.startRTM.reset()
         plugin = {
           init: sinon.stub()
-        };
-      });
+        }
+      })
 
-      it('should initialize one plugin not in an array', function() {
-        skellington({plugins: plugin, port: 1234});
-        botMock.startRTM.args[0][0](null, connectedBotMock);
+      it('should initialize one plugin not in an array', function () {
+        skellington({plugins: plugin, port: 1234})
+        botMock.startRTM.args[0][0](null, connectedBotMock)
 
-        expect(plugin.init).to.have.been.calledWith(controllerMock, connectedBotMock, expressAppMock);
-      });
+        expect(plugin.init).to.have.been.calledWith(controllerMock, connectedBotMock, expressAppMock)
+      })
 
-      it('should initialize one plugin in an array', function() {
-        skellington({plugins: [plugin], port: 1234});
-        botMock.startRTM.args[0][0](null, connectedBotMock);
+      it('should initialize one plugin in an array', function () {
+        skellington({plugins: [plugin], port: 1234})
+        botMock.startRTM.args[0][0](null, connectedBotMock)
 
-        expect(plugin.init).to.have.been.calledWith(controllerMock, connectedBotMock, expressAppMock);
-      });
+        expect(plugin.init).to.have.been.calledWith(controllerMock, connectedBotMock, expressAppMock)
+      })
 
-      it('should initialize multiple plugins', function() {
+      it('should initialize multiple plugins', function () {
         let anotherExternalBot = {
           init: sinon.stub()
-        };
+        }
 
-        skellington({plugins: [plugin, anotherExternalBot], port: 1234});
-        botMock.startRTM.args[0][0](null, connectedBotMock);
+        skellington({plugins: [plugin, anotherExternalBot], port: 1234})
+        botMock.startRTM.args[0][0](null, connectedBotMock)
 
-        expect(plugin.init).to.have.been.calledWith(controllerMock, connectedBotMock, expressAppMock);
-        expect(anotherExternalBot.init).to.have.been.calledWith(controllerMock, connectedBotMock, expressAppMock);
-      });
-    });
-  });
+        expect(plugin.init).to.have.been.calledWith(controllerMock, connectedBotMock, expressAppMock)
+        expect(anotherExternalBot.init).to.have.been.calledWith(controllerMock, connectedBotMock, expressAppMock)
+      })
+    })
+  })
 
-  describe('register help listeners', function() {
-    let plugin;
-    let messageMock;
+  describe('register help listeners', function () {
+    let plugin
+    let messageMock
 
-    beforeEach(function() {
-      botMock.startRTM.reset();
+    beforeEach(function () {
+      botMock.startRTM.reset()
       plugin = {
         init: sinon.stub()
-      };
+      }
 
       messageMock = {
         team: 'crystal',
         channel: 'blue',
         user: 'persuasion'
-      };
-    });
+      }
+    })
 
-    it('should register a listener if no plugins have help text', function() {
-      skellington({plugins: [plugin]});
-      expect(controllerMock.hears).to.have.been.calledOnce;
+    it('should register a listener if no plugins have help text', function () {
+      skellington({plugins: [plugin]})
+      expect(controllerMock.hears).to.have.been.calledOnce
 
-      let callback = controllerMock.hears.args[0][2];
+      let callback = controllerMock.hears.args[0][2]
 
-      callback(botMock, messageMock);
-      expect(botMock.reply).to.have.been.calledWithMatch(messageMock, /^I can't help you/);
-    });
+      callback(botMock, messageMock)
+      expect(botMock.reply).to.have.been.calledWithMatch(messageMock, /^I can't help you/)
+    })
 
-    it('should register a listener for each plugin with help text', function() {
+    it('should register a listener for each plugin with help text', function () {
       plugin.help = {
         command: 'rick',
         text: 'sanchez'
-      };
+      }
 
       let anotherPlugin = {
         init: sinon.stub(),
@@ -177,80 +175,80 @@ describe('Skellington', function() {
           command: 'morty',
           text: 'smith'
         }
-      };
+      }
 
-      let aThirdPlugin = {init: sinon.stub()};
+      let aThirdPlugin = {init: sinon.stub()}
 
-      skellington({plugins: [plugin, anotherPlugin, aThirdPlugin]});
+      skellington({plugins: [plugin, anotherPlugin, aThirdPlugin]})
 
-      expect(controllerMock.hears.callCount).to.equal(3); // once for general help, twice for plugins with help text
+      expect(controllerMock.hears.callCount).to.equal(3) // once for general help, twice for plugins with help text
 
       // call all the callbacks
-      _.forEach(controllerMock.hears.args, function(args) {
-        args[2](botMock, messageMock);
-      });
+      _.forEach(controllerMock.hears.args, function (args) {
+        args[2](botMock, messageMock)
+      })
 
-      expect(botMock.reply).to.have.been.calledWithMatch(messageMock, /^Here are some things/);
-      expect(botMock.reply).to.have.been.calledWith(messageMock, plugin.help.text);
-      expect(botMock.reply).to.have.been.calledWith(messageMock, anotherPlugin.help.text);
-    });
+      expect(botMock.reply).to.have.been.calledWithMatch(messageMock, /^Here are some things/)
+      expect(botMock.reply).to.have.been.calledWith(messageMock, plugin.help.text)
+      expect(botMock.reply).to.have.been.calledWith(messageMock, anotherPlugin.help.text)
+    })
 
-    it('should handle a plugin help text callback', function() {
-      let helpTextCb = sinon.stub();
+    it('should handle a plugin help text callback', function () {
+      let helpTextCb = sinon.stub()
 
       plugin.help = {
         command: 'walter',
         text: helpTextCb
-      };
+      }
 
-      botMock.identity = {name: 'rickandmorty'};
+      botMock.identity = {name: 'rickandmorty'}
 
-      skellington({plugins: [plugin]});
+      skellington({plugins: [plugin]})
 
       // call all the callbacks
-      _.forEach(controllerMock.hears.args, function(args) {
-        args[2](botMock, messageMock);
-      });
+      _.forEach(controllerMock.hears.args, function (args) {
+        args[2](botMock, messageMock)
+      })
 
       expect(helpTextCb).to.have.been.calledWith({
         botName: botMock.identity.name,
         team: messageMock.team,
         channel: messageMock.channel,
         user: messageMock.user
-      });
-    });
-  });
+      })
+    })
+  })
 
-  describe('rtm_close', function() {
-    let callback;
+  describe('rtm_close', function () {
+    let callback
 
-    beforeEach(function() {
-      skellington({});
+    beforeEach(function () {
+      skellington({})
 
-      controllerMock.log.reset();
+      controllerMock.log.reset()
 
-      botMock.startRTM.reset();
-      botMock.startRTM.yields(null);
+      botMock.startRTM.reset()
+      botMock.startRTM.yields(null)
 
-      callback = controllerMock.on.args[0][1];
-    });
+      callback = controllerMock.on.args[0][1]
+    })
 
-    it('should reconnect', function() {
-      callback(botMock);
-      expect(controllerMock.log).to.be.calledOnce;
-      expect(botMock.startRTM).to.be.calledOnce;
-    });
+    it('should reconnect', function () {
+      callback(botMock)
+      expect(controllerMock.log).to.be.calledOnce
+      expect(botMock.startRTM).to.be.calledOnce
+    })
 
-    it('should exit if reconnect fails', function() {
-      let error = new Error('GAZORPAZORP');
+    it('should exit if reconnect fails', function () {
+      let error = new Error('GAZORPAZORP')
 
-      botMock.startRTM.yields(error);
+      botMock.startRTM.yields(error)
 
-      callback(botMock);
-      expect(botMock.startRTM).to.be.calledOnce;
-      expect(controllerMock.log.callCount).to.equal(3);
-      expect(controllerMock.log.args[2][0]).to.equal(error);
-      expect(process.exit).to.be.calledWith(1);
-    });
-  });
-});
+      callback(botMock)
+      expect(botMock.startRTM).to.be.calledOnce
+      expect(controllerMock.log.callCount).to.equal(3)
+      expect(controllerMock.log.args[2][0]).to.equal(error)
+      expect(process.exit).to.be.calledWith(1)
+    })
+  })
+})
