acc239898165d5eac0512e4aba8cc50fa4369a7c
Cole Furfaro-Strode <colestrode@gmail.com>
2016-10-05 12:13:40 -0400

    adds botConnected callback

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ module.exports = (config) => {
   addHelpListeners(controller, config.plugins)
   startServer(config, controller)
 
+  // TODO single bot flow
   let bot = controller.spawn({
     token: config.slackToken
   })
-
   startRtm(config, controller, bot)
 
   // restart if disconnected
@@ -102,6 +102,13 @@ function registerHelpListener (controller, helpInfo) {
   })
 }
 
+/**
+ * Starts an RTM connection for a bot
+ *
+ * @param config
+ * @param controller
+ * @param bot
+ */
 function startRtm (config, controller, bot) {
   bot.startRTM((err, connectedBot) => {
     if (err) {
@@ -109,9 +116,49 @@ function startRtm (config, controller, bot) {
       return process.exit(1) // need the return for tests which mock our process.exit
     }
 
-    _.forEach(config.plugins, (plugin) => {
-      plugin.init(controller, connectedBot, controller.webserver)
-    })
+    // TODO only call initializePlugins here for a single bot instance
+    initializePlugins(config.plugins, controller, connectedBot)
+    botConnected(config.plugins, controller, connectedBot)
+  })
+}
+
+/**
+ * Initializes plugins, called on Skellington initialization
+ *
+ * @param plugins
+ * @param controller
+ * @param bot
+ */
+function initializePlugins (plugins, controller, bot) {
+  bot = bot || null // bot may be undefined for multi-teams
+  _.forEach(plugins, (plugin) => {
+    if (_.isFunction(plugin.init)) {
+      // this interface is deprecated, but keep it for legacy bots
+      try {
+        plugin.init(controller, bot, controller.webserver)
+      } catch (err) {
+        logError(controller, err, 'error calling init on plugin')
+      }
+    }
+  })
+}
+
+/**
+ * Calls plugin botConnected callbacks when a bot is added
+ *
+ * @param plugins
+ * @param controller
+ * @param bot
+ */
+function botConnected (plugins, controller, bot) {
+  _.forEach(plugins, (plugin) => {
+    if (_.isFunction(plugin.botConnected)) {
+      try {
+        plugin.botConnected(controller, bot)
+      } catch (err) {
+        logError(controller, err, 'error calling botConnected on plugin')
+      }
+    }
   })
 }
 
